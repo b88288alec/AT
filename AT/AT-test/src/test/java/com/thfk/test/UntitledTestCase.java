ea3e11623110bf46d3b85df9fe72bd861508b845
Alec.Tsui <alec.tsui@hy-tech.com.tw>
2018-03-05 12:25:21 +0800

    [Testing] 偵測OS取得相應的webDriver

--- AT/AT-test/src/test/java/com/thfk/test/UntitledTestCase.java
+++ AT/AT-test/src/test/java/com/thfk/test/UntitledTestCase.java
@@ -13,66 +13,78 @@ import org.openqa.selenium.NoAlertPresentException;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class UntitledTestCase {
-  private WebDriver driver;
-  private String baseUrl;
-  private boolean acceptNextAlert = true;
-  private StringBuffer verificationErrors = new StringBuffer();
+	private WebDriver driver;
+	private String baseUrl;
+	private boolean acceptNextAlert = true;
+	private StringBuffer verificationErrors = new StringBuffer();
 
-  @Before
-  public void setUp() throws Exception {
-	System.setProperty("webdriver.chrome.driver", "src/test/resources/webdriver/chromedriver_linux64");
-	driver = new ChromeDriver();
-    baseUrl = "https://www.katalon.com/";
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-  }
+	@Before
+	public void setUp() throws Exception {
+		driver = getDriver();
+		baseUrl = "https://www.katalon.com/";
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
 
-  @Test
-  public void testUntitledTestCase() throws Exception {
-    driver.get("http://192.168.198.1:8080/AT-web/");
-    driver.findElement(By.id("click")).click();
-  }
+	@Test
+	public void testUntitledTestCase() throws Exception {
+		driver.get("http://192.168.198.1:8080/AT-web/");
+		driver.findElement(By.id("click")).click();
+	}
 
-  @After
-  public void tearDown() throws Exception {
-    driver.quit();
-    String verificationErrorString = verificationErrors.toString();
-    if (!"".equals(verificationErrorString)) {
-      fail(verificationErrorString);
-    }
-  }
+	@After
+	public void tearDown() throws Exception {
+		driver.quit();
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
+	}
 
-  private boolean isElementPresent(By by) {
-    try {
-      driver.findElement(by);
-      return true;
-    } catch (NoSuchElementException e) {
-      return false;
-    }
-  }
+	private boolean isElementPresent(By by) {
+		try {
+			driver.findElement(by);
+			return true;
+		} catch (NoSuchElementException e) {
+			return false;
+		}
+	}
 
-  private boolean isAlertPresent() {
-    try {
-      driver.switchTo().alert();
-      return true;
-    } catch (NoAlertPresentException e) {
-      return false;
-    }
-  }
+	private boolean isAlertPresent() {
+		try {
+			driver.switchTo().alert();
+			return true;
+		} catch (NoAlertPresentException e) {
+			return false;
+		}
+	}
 
-  private String closeAlertAndGetItsText() {
-    try {
-      Alert alert = driver.switchTo().alert();
-      String alertText = alert.getText();
-      if (acceptNextAlert) {
-        alert.accept();
-      } else {
-        alert.dismiss();
-      }
-      return alertText;
-    } finally {
-      acceptNextAlert = true;
-    }
-  }
+	private String closeAlertAndGetItsText() {
+		try {
+			Alert alert = driver.switchTo().alert();
+			String alertText = alert.getText();
+			if (acceptNextAlert) {
+				alert.accept();
+			} else {
+				alert.dismiss();
+			}
+			return alertText;
+		} finally {
+			acceptNextAlert = true;
+		}
+	}
+
+	private WebDriver getDriver() {
+		String os = System.getProperty("os.name");
+		if (os.indexOf("Windows") != -1) {
+			System.setProperty("webdriver.chrome.driver", "src/test/resources/webdriver/chromedriver.exe");
+		} else if (os.indexOf("Linux") != -1) {
+			System.setProperty("webdriver.chrome.driver", "src/test/resources/webdriver/chromedriver_linux64");
+		}
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--no-sandbox");
+		return new ChromeDriver(options);
+	}
 }
